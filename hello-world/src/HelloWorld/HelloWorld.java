2ca9f4f2677f820e6218a2dbb4c3b825b1b9f2e2
jprince <jprince@127.0.0.1>
2017-10-06 15:25:18 +0100

    First test commit into github

---
+++ hello-world/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package HelloWorld;
+
+/**
+ * @author jprince
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hello-world2/src/helloworld/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author jprince
+ *
+ */
+package helloworld;
